c30f98db3b31af12f98daac7d2cd4e9bb9c115a7
DrewNuschler <lookup1929@gmail.com>
2025-03-05 01:20:37 -0600

    finished

--- Index.js
+++ Index.js
@@ -6,5 +6,24 @@ function workChecker(){
     pay = prompt("How much do they pay?");
     workHeader.innerHTML = `Oh ${work}, Do you like it there?.`;
     workParagraph.innerHTML = `${pay}, thats not bad when you enjoy your job.`;
+
+ workButton.onclick = workChecker;
 }
-workButton.onclick = workChecker;
+
+function calculateWeekly() {
+    let hours;
+    let payRate;
+    let weekly;
+    hours = prompt("How many hours do you work?");
+    payRate = prompt("What is your hourly rate?");
+    
+    
+    let hoursWorked = Number(hours);
+    let payPerHour = Number(payRate);
+    weekly = hoursWorked * payPerHour;
+    
+    monthlyParagraph.innerHTML = `${weekly} is your total monthly income.`;
+
+     }
+  
+  calculateButton.onclick = calculateWeekly;
